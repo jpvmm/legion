c29a928cffd405df8d34f43d7512506ab0cd2b59
jpvirgilio <jpvirgilio@indracompany.com>
2018-10-09 13:41:12 -0300

    funcoes para processamento das linhas

---
+++ data_preparator.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from torchnlp import text_encoders
+from torchnlp.utils import pad_tensor
+
+def load_dataset(path):
+    df = pd.read_csv(path, dtype=str)
+
+    tst = df[df.Label != '0']
+
+    utterance = []
+    allo = []
+    tmpList = []
+    labels = []
+    for index, row in tst.iterrows():
+        allo.append(row['Conversa'])
+        labels.append(row['Label'])
+        if row['Unnamed: 0'] == '0':
+            if len(tmpList) != 0:
+                utterance.append(tmpList)
+                tmpList = []
+
+        tmpList.append({'Conversa': row['Conversa'], 'Label': row['Label']})
+
+    return allo, labels
+
+def encode_text(lines):
+    encoded = []
+
+    #Cria o encoder
+    encoder = text_encoders.StaticTokenizerEncoder(lines, tokenize=lambda s: s.split())
+
+    #Cria iterator para o encoder
+    gen = (i for i in lines)
+    encoded.append(encoder.encode(gen.__next__()))
+
+    #Faz o padding de cada linha nos dialogos para um tamanho maximo de 90
+    padded = [pad_tensor(encoded[cvs], length=90) for cvs in range(len(encoded))]
+
+def prepare_dataset(path):
+    lines, labels = load_dataset(path)
+
+    padded = encode_text(lines)
+
+    return padded, labels
